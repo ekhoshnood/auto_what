0405d2d5702bb20ff750d5b85475851c7aebe701
ekhoshnood <e.khoshnood966@gmail.com>
2019-11-04 17:24:21 +0330

    requirement

--- auto_what/urls.py
+++ auto_what/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views as Vmain
 
 
 urlpatterns = [
+    path('', Vmain.home_screen),
     path('admin/', admin.site.urls),
 ]
