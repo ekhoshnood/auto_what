ab9caef815fd904e62c533fd9e4f76afd8660331
ekhoshnood <e.khoshnood966@gmail.com>
2019-11-04 22:33:01 +0330

    html done

--- personal/views.py
+++ personal/views.py
@@ -2,4 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 def personal_page(request):
-    return render(request, "personal/personal_home_screen.html", {})
+    ##first way
+    # context = {}
+    # context['some_string'] = "this is the context string that's been pass to html"
+    # context['some_number'] = 13900404
+
+    ##second way
+    # context = {
+    #     'some_string' : "this is the context string that's been pass to html",
+    #     'some_number' : 13900404
+    # }
+
+    # another thing
+    list_of_values = []
+    list_of_values.append("first")
+    list_of_values.append("second")
+    list_of_values.append("third")
+    list_of_values.append("fourth")
+    list_of_values.append("fifth")
+
+    context = {}
+    context['my_list'] = list_of_values
+
+
+    return render(request, "personal/personal_home_screen.html", context)
