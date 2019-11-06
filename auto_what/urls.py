a1bbabe1111f08a05f5b6862c8e579ca0e0683da
ekhoshnood <e.khoshnood966@gmail.com>
2019-11-06 19:27:09 +0330

    personal model
    account
    
    registration form

--- auto_what/urls.py
+++ auto_what/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import home_screen
+from account.views import registration_view
 
 
 urlpatterns = [
     path('', home_screen, name="home"),
     path('admin/', admin.site.urls),
     path('training', include('course_training.urls')),
-    path('shakhsi', include('personal.urls'))
+    path('shakhsi', include('personal.urls')),
+    path('register/', registration_view, name="register")
 ]
