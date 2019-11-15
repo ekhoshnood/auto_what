37d73ed82e6a77d9f4e19b186587f7b9b80ab374
ekhoshnood <e.khoshnood966@gmail.com>
2019-11-15 11:35:17 +0330

    personal model
    account
    
    registration form
    login form
    
    bootstrap
    
    reset form
    
    blog

--- redditpage/views.py
+++ redditpage/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from blog.models import BlogPost
+from operator import attrgetter
 
 
 def reddit(request):
-    return render(request, "reddit/home.html")
+    context={}
+    # sorted will sort the list based on the key date updated and we will reverse it to show newest post on top
+    blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True)
+    context['blog_posts'] = blog_posts
+
+    return render(request, "reddit/home.html", context)
